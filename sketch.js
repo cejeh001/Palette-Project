413bd645e35df7652254f5451ae5ff6612f68947
Chibueze <149211316+Mel0nWater@users.noreply.github.com>
2025-02-13 22:46:48 +0000

    `Added HTML5 boilerplate to index.html`

---
+++ sketch.js
@@ -0,0 +1,18 @@
+// establish colour variables
+let startCol, tarCol;
+let startx, starty;
+let divisions
+
+function setup() {
+    createCanvas(500, 500);
+    colorMode(HSB)
+    startCol = color(100, 100, 200)
+    tarCol = color(200, 100, 100)
+    startx = 
+    starty = 
+
+}
+
+function draw() {
+    background(220);
+}
