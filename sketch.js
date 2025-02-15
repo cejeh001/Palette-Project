7f54c5d1f6a35a8ca43e7019ae4d9dc622d0896e
Chibueze <149211316+Mel0nWater@users.noreply.github.com>
2025-02-15 13:36:54 +0000

    `Refactor sketch.js and style.css to add new color palette functionality`

--- sketch.js
+++ sketch.js
@@ -3,19 +3,45 @@ let startCol, tarCol;
 let startx, starty;
 let divisions;
 
+
 function setup() {
     createCanvas(500, 500);
     background(220);
 
     colorMode(HSB);
-    startCol = color(random(360), 100, 100);
-    tarCol = color(random(360), 100, 100);
+    startCol = color(random(360), 75, 80);
+    tarCol = color(random(360), 75, 80);
     startx = 50;
     starty = 50;
-    divisions = 100;
+    divisions = 50;
 }
 
-function draw() {
+function drawPalette() {
+    // create pallete section
+    fill(255);
+    noStroke();
+    rect(startx, starty , 200, 50);
+    // Loop division number of times
+    for (let i = 0; i < divisions; i+=4) {
+        // Gradient
+        let gradient = lerpColor(startCol, tarCol, i / divisions);
+        let x = lerp(startx, startx + 200, i / divisions);
+
+        // Draw
+        stroke(gradient);
+        strokeWeight(12);
+        // Draw a vertical lin from up to down
+        line(x, starty, x, starty + 50);
+    }
+}
+
+function drawAnalogousPalette() {
+
+
+    let num = random(360)
+    startCol = color(num, 100, 100);
+    tarCol = color(num + 30, 100, 100);
+
     // Loop division number of times
     for (let i = 0; i < divisions; i++) {
         // Gradient
@@ -24,8 +50,14 @@ function draw() {
 
         // Draw
         stroke(gradient);
-        strokeWeight(3);
+        strokeWeight(10);
         // Draw a vertical line from up to down
         line(x, starty, x, starty + 50);
     }
+}
+
+// draw normal palette function
+function draw() {
+    drawPalette();
+    drawAnalogousPalette(); 
 }
